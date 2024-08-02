94d266ac64c4c90ad9362cbd6d78e8dec0656c55
Hizar62 <hizarsajjad632@gmail.com>
2024-08-03 01:59:56 +0500

    fixing bugs

--- recordLead/views.py
+++ recordLead/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import LeadRegistration
+from .models import Lead
 
 # Create your views here.
 def add_Lead(request):
@@ -14,7 +15,10 @@ def add_Lead(request):
     return render(request,'recordLead/add.html', {'form':fm})
 
 def show_Lead(request):
-    return render(request, 'recordLead/show.html')
+    fm = LeadRegistration()  # Create the form instance
+    leads = Lead.objects.all()  # Query all leads
+    return render(request, 'recordLead/show.html', {'form': fm, 'leads': leads})
+
 
 def update_Lead(request):
     return render(request, 'recordLead/update.html')
